9e2840df24646ab2edd70dd344028be8fbf761e1
zillur-rgb <zillurdeu@gmail.com>
2022-03-21 22:50:41 +0100

    Search function complete

--- src/Components/Searched.jsx
+++ src/Components/Searched.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Wrapper, Card } from "./StyledComponents/styled.style";
 
 const Searched = () => {
+  const [searchedRecipes, getSearchedRecipes] = useState([]);
+  const params = useParams();
+
+  const getSearched = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=9adf169d12924ead98d0e180e8d6e8f1&number=9&query=${name}`
+    );
+
+    const res = await data.json();
+    getSearchedRecipes(res.results);
+  };
+
+  useEffect(() => {
+    getSearched(params.tags);
+  }, [params.tags]);
+
   return (
     <div>
       <h1>Search Result</h1>
+
+      <Wrapper>
+        {searchedRecipes.map((recipe) => {
+          return (
+            <div>
+              <Card key={recipe.id}>
+                <img src={recipe.image} alt="" />
+              </Card>
+              <h4>{recipe.title}</h4>
+            </div>
+          );
+        })}
+      </Wrapper>
     </div>
   );
 };
